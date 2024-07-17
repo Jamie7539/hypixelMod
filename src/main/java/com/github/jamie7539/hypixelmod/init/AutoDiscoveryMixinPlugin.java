7dfd133004abb75641824237428fd01a3c2a3810
GitHub Action <action@github.com>
2024-07-17 23:01:42 +0000

    Initialize template repository proper
    Set package name to com.github.jamie7539.hypixelmod
    Set project name to hypixelMod
    Set modid to hypixelmod

--- src/main/java/com/github/jamie7539/hypixelmod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/jamie7539/hypixelmod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.jamie7539.hypixelmod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/jamie7539/hypixelmod/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/jamie7539/hypixelmod/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.jamie7539.hypixelmod.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
